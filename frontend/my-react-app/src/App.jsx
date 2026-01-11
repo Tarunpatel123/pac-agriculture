fd2eebbf5f2aabce52d64402f58c8f237ee585bc
Tarun Patel <tp93003@gmail.com>
2026-01-11 23:31:53 +0530

    Fix: Added ScrollToTop component to reset scroll position on navigation

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -182,6 +183,7 @@ function AppContent() {
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AppContent />
     </Router>
   )
